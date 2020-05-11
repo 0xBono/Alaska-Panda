40144682328fce1e39d075e4a196af3decfe48b5
0xBono <0xbono@naver.com>
2020-05-12 01:39:08 +0900

    feat(component): add TouchableOpacity in MoreNotice screen

--- src/screens/More/MoreNotice.tsx
+++ src/screens/More/MoreNotice.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native';
 import StackNavigation from '../../components/StackNavigation';
 import {
     widthPercentageToDP as wp,
@@ -10,10 +10,12 @@ const MoreNotice: React.FC<{}> = () => (
     <SafeAreaView style={styles.Container}>
         <StackNavigation status={true} title={'공지사항'} />
         <ScrollView>
-            <View style={styles.ListContainer}>
-                <Text numberOfLines={1} style={{ width: '80%', fontWeight: 'bold' }}>테스트 공지사항입니다. 테스트입니다. 자동으로 ...이 생깁니다 확인 부탁드립니다</Text>
-                <Text style={{ marginTop: hp('1%'), color: '#C8C8C8' }}>2020. 11. 22 목요일</Text>
-            </View>
+            <TouchableOpacity activeOpacity={0.8} >
+                <View style={styles.ListContainer}>
+                    <Text numberOfLines={1} style={{ width: '80%', fontWeight: 'bold' }}>테스트 공지사항입니다. 테스트입니다. 자동으로 ...이 생깁니다 확인 부탁드립니다</Text>
+                    <Text style={{ marginTop: hp('1%'), color: '#C8C8C8' }}>2020. 11. 22 목요일</Text>
+                </View>
+            </TouchableOpacity>
         </ScrollView>
     </SafeAreaView>
 );
