4ff5091889769d1e47885b3c9f5f7b3dc848f10f
0xBono <0xbono@naver.com>
2020-05-11 19:14:21 +0900

    feat(component): development is FooterButton

---
+++ src/components/FooterButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import { scale } from '../constants/size';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+type Props = {
+  title: string;
+};
+
+const FooterButton: React.SFC<Props> = props => (
+  <TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <Text style={styles.text}>{props.title}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#DC2125',
+    width: wp('100%'),
+    height: hp('15%'),
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: scale(20),
+    lineHeight: scale(70),
+    color: '#fff',
+  },
+});
+
+export default FooterButton;
