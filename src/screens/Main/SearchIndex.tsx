009bc365777c317de351b649695349fcc6b78733
0xBono <0xbono@naver.com>
2020-05-22 23:22:00 +0900

    feat(screen): SearchIndex

---
+++ src/screens/Main/SearchIndex.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {StyleSheet, View, Text, TextInput, SafeAreaView} from 'react-native';
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Navigation from '../../components/Navigation';
+import { scale } from '../../constants/size';
+import Search from '../../assets/search';
+
+class SearchIndex extends React.Component {
+    render() {
+        return (
+            <SafeAreaView style={styles.Container}>
+                <Navigation title={'검색'} />
+                <View style={styles.MainContainer}>
+                    <Search style={styles.SearchStyle} width={25} height={25} />
+                    <TextInput style={styles.SearchBar} placeholder={'지역, 상품, 상점명 검색'} />
+                </View>
+                <View style={styles.TitleContainer}>
+                    <Text style={styles.LargeTItle}>인기 검색어</Text>
+                    <Text style={styles.SubTitle}>1. 홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>2. 홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>3. 홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>4. 홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>5. 홍대 옷가게</Text>
+                </View>
+                <View style={styles.TitleContainer}>
+                    <Text style={styles.LargeTItle}>최근 검색어</Text>
+                    <Text style={styles.SubTitle}>홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>홍대 옷가게</Text>
+                    <Text style={styles.SubTitle}>홍대 옷가게</Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+    MainContainer: {
+        alignSelf: 'center',
+    },
+    SearchBar: {
+        width: wp('87%'),
+        height: hp('6%'),
+        borderWidth: 1,
+        borderRadius: scale(5),
+        borderColor: '#c4c4c4',
+        paddingLeft: 40,
+    },
+    SearchStyle: {
+        position: 'absolute',
+        marginTop: hp('1%'),
+        marginLeft: wp('2%'),
+    },
+    TitleContainer: {
+        marginTop: hp('6%'),
+        marginLeft: wp('6%'),
+    },
+    LargeTItle: {
+        fontSize: scale(18),
+        fontWeight: 'bold'
+    },
+    SubTitle: {
+        fontSize: scale(14),
+        color: '#666666',
+        marginTop: hp('2%'),
+    }
+})
+
+export default SearchIndex;
